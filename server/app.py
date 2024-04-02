aeda5b4ae4b0cad690d870219644038643d4e9bf
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-04-02 16:20:48 -0700

    code along deployment

--- server/app.py
+++ server/app.py
@@ -27,6 +27,12 @@ db.init_app(app)
 def not_found(e):
     return render_template("index.html")
 
+
+@app.route('/')
+@app.route('/<int:id>')
+def index(id=0):
+    return render_template("index.html")
+
 api = Api(app)
 
 class Birds(Resource):
